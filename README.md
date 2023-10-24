93f3eb85a9c93198e7fc4133721040170273b73d
Sebastian <sebastian.buettner@coremedia.com>
2023-10-24 09:03:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CoreMedia Commerce Hub Connector for Commerce Layer
+
+This Commerce Hub Adapter allows to connect Commerce Layer commerce engine with CoreMedia Content Cloud via the CoreMedia Commerce Hub.
---
+++ adapter-commercelayer-app/src/main/java/com/coremedia/commerce/adapter/commercelayer/CommerceLayerConnectorApplication.java
@@ -0,0 +1,12 @@
+package com.coremedia.commerce.adapter.commercelayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceLayerConnectorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommerceLayerConnectorApplication.class, args);
+  }
+}
