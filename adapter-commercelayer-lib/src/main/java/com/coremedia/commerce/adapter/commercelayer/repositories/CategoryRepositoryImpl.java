4005c2a2453f0240a2df5e59e87a646f07d3c051
Sebastian Büttner <sebastian.buettner@coremedia.com>
2023-10-30 16:59:08 +0100

    Refactorings, adding code documentation

--- adapter-commercelayer-lib/src/main/java/com/coremedia/commerce/adapter/commercelayer/repositories/CategoryRepositoryImpl.java
+++ adapter-commercelayer-lib/src/main/java/com/coremedia/commerce/adapter/commercelayer/repositories/CategoryRepositoryImpl.java
@@ -73,7 +73,7 @@ public class CategoryRepositoryImpl implements CategoryRepository {
             // fetch category via API
             category = shippingCategoriesResource.getShippingCategory(categoryId.getValue()).map(this::toCategory);
             if (category.isEmpty()) {
-                category = skuListsResource.getSkuList(categoryId.getValue()).map(this::toSKUCategory);
+                category = skuListsResource.getSkuList(categoryId.getValue()).map(this::toCategory);
             }
         }
         return category;
@@ -88,6 +88,12 @@ public class CategoryRepositoryImpl implements CategoryRepository {
         return category;
     }
 
+    /**
+     * Convert a {@link ShippingCategory} to a Commerce Hub {@link Category} entity.
+     *
+     * @param shippingCategory the shipping category
+     * @return
+     */
     private Category toCategory(ShippingCategory shippingCategory) {
         ExternalId id = ExternalId.of(shippingCategory.getId());
         String name = shippingCategory.getAttributes().getName();
@@ -108,16 +114,23 @@ public class CategoryRepositoryImpl implements CategoryRepository {
         return categoryBuilder.build();
     }
 
-    private Category toSKUCategory(SKUList skuList) {
-        // TODO: Implement (see above #toCategory)
+    /**
+     * Convert a {@link SKUList} to a Commerce Hub {@link Category} entity.
+     *
+     * @param skuList the SKU list
+     * @return
+     */
+    private Category toCategory(SKUList skuList) {
         ExternalId id = ExternalId.of(skuList.getId());
         String name = skuList.getAttributes().getName();
         CategoryBuilder categoryBuilder = Category.builder(id, name);
         categoryBuilder.setSeoSegment(skuList.getAttributes().getReference());
         categoryBuilder.setTitle(name);
-        
+
+        // Set parent link to virtual "SKU lists" category
         categoryBuilder.setParentId(SKU_LISTS_CATEGORY_ID);
-        
+
+        // Add assigned products
         List<Id> assignedProducts = skuListsResource.getAssociatedSkus(skuList.getId()).stream()
                 .map(SKU::getId)
                 .map(ExternalId::of)
@@ -127,12 +140,19 @@ public class CategoryRepositoryImpl implements CategoryRepository {
         return categoryBuilder.build();
     }
 
+    /**
+     * Initializes the virtual categories.
+     */
     private void initVirtualCategories() {
         rootCategory = createRootCategory();
         shippingCategoriesCategory = createShippingCategoriesCategory();
         skuListsCategory = createSkuListsCategory();
     }
 
+    /**
+     * Create the root category.
+     * @return
+     */
     private Category createRootCategory() {
         CategoryBuilder categoryBuilder = Category.builder(ROOT_CATEGORY_ID, "Root");
         categoryBuilder.setChildIds(List.of(SHIPPING_CATEGORIES_CATEGORY_ID, SKU_LISTS_CATEGORY_ID));
@@ -159,11 +179,17 @@ public class CategoryRepositoryImpl implements CategoryRepository {
         return categoryBuilder.build();
     }
 
+    /**
+     * Create a virtual category as an entry point
+     * for all <a href="https://docs.commercelayer.io/core/v/api-reference/sku_lists">SKU lists</a>.
+     *
+     * @return
+     */
     private Category createSkuListsCategory() {
         CategoryBuilder categoryBuilder = Category.builder(SKU_LISTS_CATEGORY_ID, "SKU Lists");
         categoryBuilder.setParentId(rootCategory.getExternalId());
 
-        // TODO: Fetch all SKU lists and add them like we do with shipping categories (see above)
+        // Add all direct SKU lists
         List<Id> skuListsIds = skuListsResource.listSkuLists().stream()
                 .map(CommerceLayerApiEntity::getId)
                 .map(ExternalId::of)
